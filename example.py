b58c73c0ced69b0cc6ebd1e8d3d045a38ee6c4ed
git <bchinalien@outlook.com>
2025-01-30 00:17:18 +0100

    perfect reconstruction not working

--- example.py
+++ example.py
@@ -15,7 +15,7 @@ import torch
 import PIL.Image
 import dnnlib
 
-from einops import rearrange
+from einops import rearrange, repeat
 
 import matplotlib.pyplot as plt # should be in seperate function
 from matplotlib.offsetbox import AnnotationBbox, OffsetImage # should be in seperate function
@@ -29,7 +29,7 @@ def generate_image_grid(
     seed=0, gridw=2, gridh=2, device=torch.device('cuda'),
     num_steps=18, sigma_min=0.002, sigma_max=80, rho=7,
     S_churn=0, S_min=0, S_max=float('inf'), S_noise=1,
-    guide_template=0, guide_trained=1.0, second_order=True,
+    v_0=1.0, guide_template=0, guide_trained=1.0, second_order=True, 
     save_all_timesteps=False,
 ):
     batch_size = gridw * gridh
@@ -42,10 +42,8 @@ def generate_image_grid(
 
     # Import Template
     img = read_image("cat.jpg")
-    x_template = img.to(device).to(torch.float32) 
-    x_template = (x_template - torch.mean(x_template)) / torch.std(x_template) #??: Why don't we need to rescale
-    x_template = x_template.view(net.img_channels, net.img_resolution, net.img_resolution)
-    x_template = x_template.repeat(batch_size, 1, 1, 1)
+    x_template = repeat(img, "c h w -> repeat c h w", repeat=batch_size)
+    x_template = x_template.to(device).to(torch.float32) / 255
 
     # Pick latents and labels.
     print(f'Generating {batch_size} images...')
@@ -73,6 +71,7 @@ def generate_image_grid(
 
     # Main sampling loop.
     x_next = latents.to(torch.float64) * t_steps[0]
+    print(x_next.shape)
     for i, (t_cur, t_next) in tqdm.tqdm(list(enumerate(zip(t_steps[:-1], t_steps[1:]))), unit='step'): # 0, ..., N-1
         x_cur = x_next
 
@@ -80,14 +79,14 @@ def generate_image_grid(
         gamma = min(S_churn / num_steps, np.sqrt(2) - 1) if S_min <= t_cur <= S_max else 0
         t_hat = net.round_sigma(t_cur + gamma * t_cur)
         x_hat = x_cur + (t_hat ** 2 - t_cur ** 2).sqrt() * S_noise * torch.randn_like(x_cur)
-
+        
         # Euler step.
         denoised = guide_trained * net(x_hat, t_hat, class_labels).to(torch.float64) if (guide_trained != 0.0) else torch.zeros_like(x_hat)
         model_score =  (x_hat - denoised ) / t_hat
-        template_score = -t_hat * (guide_template * (x_template - x_hat)) / t_hat
+        # template_score =  guide_template * (x_template - x_hat)/(t_hat**2 + v_0)
+        template_score =  1.0 * (x_template - x_hat)
         d_cur = model_score + template_score
         x_next = x_hat + (t_next - t_hat) * d_cur
-
 
         # Apply 2nd order correction.
         if i < num_steps - 1 and second_order:
@@ -101,17 +100,51 @@ def generate_image_grid(
             mags_template[i] = torch.norm(template_score * (t_next - t_hat)) 
             mags_model[i] = torch.norm(model_score * (t_next - t_hat)) 
 
+    # todo: should add way to specify image output
     # Save image grid.
     print(f'Saving image grid to "{dest_path}"...')
     image = (x_next * 127.5 + 128).to(torch.uint8)
     image = rearrange(image, "(b1 b2) c h w -> (b1 h) (b2 w) c", b1=gridh)
     image = image.cpu().numpy()
 
-    xs = (xs * 127.5 + 128).to(torch.uint8)
-    xs2 = rearrange(xs, "t (b1 b2) c  h w -> (b1 h) (t b2 w) c", b1=gridh)
-    xs2 = xs.cpu().numpy()
-    
-    # Plot magnitudes !! MOVE TO SEPEARTE FUNCTION
+    # image_over_t = (xs * 127.5 + 128).to(torch.uint8)
+    image_over_t = rearrange(xs.cpu().numpy() , "t (b1 b2) c h w -> (t b1 h) (b2 w) c", b1=gridh)
+    PIL.Image.fromarray(image_over_t, 'RGB').save(dest_path)
+
+    plt.imshow(image_over_t)
+    plt.show()
+
+
+    print('Done.')
+
+#----------------------------------------------------------------------------
+
+def main():
+    model_root = 'https://nvlabs-fi-cdn.nvidia.com/edm/pretrained'
+    second_order = False
+    num_steps = 32
+
+    # for guide_template in np.power(10, np.linspace(-2, 0, 20)):
+    for guide_template in [1.0]:
+        v_0 = 1.0
+        
+        fname = f'imgs/imgnet-numsteps_{num_steps}-v0_{v_0:.2f}-gtmp_{guide_template:.6f}-secondorder_{second_order}.png'
+        generate_image_grid(f'{model_root}/edm-imagenet-64x64-cond-adm.pkl', fname,
+                            seed=0,  guide_template=guide_template, guide_trained=1.0-guide_template, v_0=v_0,
+                            num_steps=num_steps, second_order=second_order,
+                            S_churn=0, S_min=0.05, S_max=50, S_noise=1.003,  # default S_churn=40, S_churn=0 turns off adding noise
+                            gridw=2, gridh=2, save_all_timesteps=True,
+                        ) 
+
+
+#----------------------------------------------------------------------------
+
+if __name__ == "__main__":
+    main()
+
+#----------------------------------------------------------------------------
+
+def plot_magnitude():
     fig, ax  = plt.subplots(1, 1)
     ax.scatter(list(range(0, num_steps)), np.log10(mags_template.cpu()), label="Template")
     ax.scatter(list(range(0, num_steps)), np.log10(mags_model.cpu()), label="Model")
@@ -128,32 +161,3 @@ def generate_image_grid(
         ab = AnnotationBbox(im, (i, min_y - 1), frameon=False)
         ax.add_artist(ab)
 
-    
-    plt.show()
-
-    # PIL.Image.fromarray(image, 'RGB').save(dest_path)
-    # PIL.Image.fromarray(xs2, 'RGB').save(dest_path)
-    print('Done.')
-
-#----------------------------------------------------------------------------
-
-def main():
-    model_root = 'https://nvlabs-fi-cdn.nvidia.com/edm/pretrained'
-    second_order = False
-    num_steps = 32
-
-    for guide_template in [0.08]:
-        fname = f'imgs/imgnet-numsteps_{num_steps}-gtmp_{guide_template:.3f}-secondorder_{second_order}.png'
-        generate_image_grid(f'{model_root}/edm-imagenet-64x64-cond-adm.pkl', fname,
-                            seed=0,  guide_template=guide_template, guide_trained=1.0 - guide_template,
-                            num_steps=num_steps, second_order=second_order,
-                            S_churn=40, S_min=0.05, S_max=50, S_noise=1.003, 
-                            gridw=1, gridh=1, save_all_timesteps=True,
-                        ) 
-
-#----------------------------------------------------------------------------
-
-if __name__ == "__main__":
-    main()
-
-#----------------------------------------------------------------------------
