d1a51969260f0765bedd3a8510c9b6f158688782
M-wag <bchinalien@outlook.com>
2025-05-06 15:06:42 +0200

    Added config for two (nested) conditons

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import numpy as np
+import itertools
 import matplotlib.pyplot as plt
 import os 
 import torch
@@ -5,17 +7,34 @@ import pickle
 from einops import rearrange
 from dataclasses import replace
 from mylib.diffusion import schedule_diffusion, ConfigSimulation, ConfigDiffusion, ConfigGuidanceVF
-
+import math 
 
 MODEL_ROOT = 'https://nvlabs-fi-cdn.nvidia.com/edm/pretrained'
-CNFG_NO_GUIDANCE = ConfigSimulation(
-        network_pkl     = f'{MODEL_ROOT}/edm-imagenet-64x64-cond-adm.pkl',
+
+CNFG_NO_GUIDANCE = ConfigSimulation( 
+        network_pkl     = f'{MODEL_ROOT}/edm-imagenet-64x64-cond-adm.pkl', 
         device          = "cuda" if torch.cuda.is_available() else "cpu",
         seed            = 0,
         input_shape     = (3, 64, 64),
         guidance_vf     = None,
         diffusion       = ConfigDiffusion(num_steps=32),
         )
+
+CNFG_SCALE_AND_V0 = ConfigSimulation( 
+        network_pkl     = f'{MODEL_ROOT}/edm-imagenet-64x64-cond-adm.pkl', 
+        device          = "cuda" if torch.cuda.is_available() else "cpu",
+        seed            = 0,
+        input_shape     = (3, 64, 64),
+        guidance_vf     = ConfigGuidanceVF(
+                            vf_type = "pixel",
+                            decay_rate = 1.0,
+                            v_0 = [15, 30],
+                            scale_template_score = [0.5, 1.0],
+                            template_path = "data/input/cat.jpg",
+                          ),
+        diffusion       = ConfigDiffusion(num_steps=32),
+        )
+
 
 def run_no_guidance(exp_name, cnfg):
     # Set output destination
@@ -46,21 +65,56 @@ def run_no_guidance(exp_name, cnfg):
     return path_exp
 
 if __name__ == "__main__":
-    cnfg = CNFG_NO_GUIDANCE(
-            seed = 1,
-            diffusion=CNFG_NO_GUIDANCE.diffusion(num_steps=32, batch_size=1))
-    path_exp =  run_no_guidance("no_guidance", cnfg)
+    RUN_FRESH = True
+
+    if RUN_FRESH:
+        cnfg = CNFG_SCALE_AND_V0(
+                diffusion = CNFG_SCALE_AND_V0.diffusion(num_steps=32, batch_size=3)
+        )
+
+        path_exp =  run_no_guidance("gamma_and_v0", cnfg)
+    else:
+        path_exp = os.path.join(os.getcwd(), "data", "output", "gamma_and_v0_18")
 
     raw_data_path = os.path.join(path_exp, "raw_data.pkl")
     cnfg_sim_path = os.path.join(path_exp, "cnfg_sim.pkl")
+
     with open(raw_data_path, "rb") as f:
         raw_data = pickle.load(f)
+    with open(cnfg_sim_path, "rb") as f:
+        cnfg_sim = pickle.load(f)
+    assert len(raw_data.shape) == 6, f"raw_data should have rank 6, got shape : {raw_data.shape}"
 
-    data = raw_data[0, 0, -1]
-    data_reshaped = rearrange(data, "(b1 b2) c h w -> (b1 h) (b2 w) c", b1=1)
-    plt.imshow(data_reshaped)
+    # Reshape to be able to pick combination
+    if len(cnfg_sim.shape_combination) == 0:
+        shape_comb = (1, 1)
+    elif len(cnfg_sim.shape_combination) == 1:
+        shape_comb = (len(cnfg_sim.shape_combination), 1)
+    else :
+        shape_comb = cnfg_sim.shape_combination 
+
+    # Pick combination index, time index and batch index
+    idx_combinations = (slice(None), slice(None))
+    idx_time = (-1, )
+    idx_batch = (slice(None),)
+
+    # Reshape data for visualize 
+    data = raw_data.reshape(shape_comb + raw_data.shape[1:])
+    data = data[idx_combinations + idx_time + idx_batch]
+    assert len(data.shape) == 6, f"data should have rank 5, got shape: {data.shape}"
+    data = rearrange(data, "p1 p2 b C H W -> b (p1 H) (p2 W) C", p1=shape_comb[0], p2=shape_comb[1])
+
+    print(data.shape)
+    
+    # Make subplot layout
+    n_cols = math.ceil(math.sqrt(cnfg_sim.diffusion.batch_size))
+    n_rows = math.ceil(cnfg_sim.diffusion.batch_size / n_cols)
+
+    fig, axes = plt.subplots(n_rows, n_cols, squeeze=False)
+    for idx, (row, col) in enumerate(np.ndindex((n_rows, n_cols))):
+        if idx == cnfg_sim.diffusion.batch_size:
+            break
+        axes[row, col].imshow(data[idx])
+
     plt.show()
-
     
-    
-
