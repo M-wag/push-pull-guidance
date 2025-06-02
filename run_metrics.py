d33118fa0ea7b78f243b561f4be476f786a96986
M-wag <bchinalien@outlook.com>
2025-06-02 11:40:03 +0200

    Added reference statistics for EDM-1 Imagenet

--- run_metrics.py
+++ run_metrics.py
@@ -2,8 +2,8 @@ import calculate_metrics
 
 if __name__ == "__main__":
     metrics = calculate_metrics.calculate_metrics_from_generator(
-        network_pkl="path/to/model.pkl",
-        ref_path="path/to/ref_stats.pkl",
+        network_pkl="https://nvlabs-fi-cdn.nvidia.com/edm/pretrained/edm-imagenet-64x64-cond-adm.pkl",
+        ref_path="data/refs/edm-1-imagenet-64x64.npz",
         num_images=50
     )
 
