a02c0b38f151b606e08defd2adb41f37aee24663
Brandon Chin-A-Lien <bchinalien@outlook.com>
2025-09-23 14:58:55 +0200

    Compose one image

--- config_runner.py
+++ config_runner.py
@@ -11,7 +11,7 @@ from PIL import Image
 from run_metrics import ExperimentRunner
 from torch_utils import distributed as dist
 from training.networks import update_EDM
-from imgviz import compose_images_batched
+from imgviz import compose_images, compose_images_batched
 
 #----------------------------------------------------------------------------
 # Continously runs a network for gennerating images 
@@ -35,7 +35,7 @@ def main():
     # Setup runner 
     num_images = 5
     paths = {
-        "config"    : "configs/test.py",
+        "config"    : "configs/config_runner.py",
         "templates" : "data/images/examples",
         "out"       : "data/images/config_runner"
     }
@@ -76,10 +76,10 @@ def main():
                 edited = [Image.fromarray(arr.permute(1, 2, 0).cpu().numpy(), "RGB") for arr in edited_images]
 
                 # Compose and save
-                compositions = compose_images_batched([original, edited, examples], "h")
+                compositions = compose_images_batched([edited, examples], "h")
+                compositions = compose_images(compositions, "v")
+                compositions.save(os.path.join(paths["out"], f"out.png"))  
                 
-                for i, comp in enumerate(compositions):
-                    comp.save(os.path.join(paths["out"], f"{i:06d}.png"))  
 
         except KeyboardInterrupt:
             if dist.get_rank() == 0:
