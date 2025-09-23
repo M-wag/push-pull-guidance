e3f18503f47160da97d1c22baa88c544eb07587d
Brandon Chin-A-Lien <bchinalien@outlook.com>
2025-09-23 20:55:55 +0200

    Update

--- configs/gvf_sd.py
+++ configs/gvf_sd.py
@@ -1,26 +1,39 @@
 import torch
 
-gradient_kwargs = {
-        "scale_model_score" : 1.0, 
-} 
 
-sampler_kwargs = {
-        "num_steps"         : 64, 
-        "sigma_min"         : 0.002  , 
-        "sigma_max"         : 80, 
-        "rho"               : 7, 
-        # "S_churn"           : 0.0,  
-        # "S_min"             : 0.0, 
-        # "S_max"             : float('inf'), 
-        # "S_noise"           : 1.0,
+determinstic_sampling = {
+        "S_churn"           : 0.0,  
+        "S_min"             : 0.0, 
+        "S_max"             : float('inf'), 
+}
+
+stochastic_sampling = {
         "S_churn"           : 40.0,
         "S_min"             : 0.05, 
         "S_max"             : 50,
-        "S_noise"           : 1.003, 
-        "dtype"             : torch.float32,
-        "correct_rgb"       : False,
+}
+
+first_order = {
+        "num_steps"         : 64,
         "apply_2nd_order"   : False,
 }
+
+second_order = {
+        "num_steps"         : 32,
+        "apply_2nd_order"   : True,
+}
+
+sampler_kwargs = {
+        "sigma_min"         : 0.002 , 
+        "sigma_max"         : 80, 
+        "rho"               : 7, 
+        "S_noise"           : 1.0,
+        "dtype"             : torch.float32,
+        "correct_rgb"       : False,
+        **second_order, 
+        **determinstic_sampling,
+}
+
 
 
 gvf_kwargs = {
@@ -36,7 +49,7 @@ gvf_kwargs = {
         },
         "noise" : "edm",
         "dtype" : torch.float32,
-        "scale"         : 1.0, 
+        "scale" : 1.0, 
         "pullback" : {"step_size_slope" : 1, "step_size_intercept": 0},
         "args_references" : {
             "features_template" : torch.zeros(1, 0, 0, 0),
@@ -44,9 +57,12 @@ gvf_kwargs = {
 }
 
 generate_kwargs = {
-        "num_images"            : 10_000,
         "ddim_inversion"        : False,
         "live_editing"          : False,
         "use_noisy_examples"    : False,
+        "example_idx_range"     : [0, 1]
 }
 
+gradient_kwargs = {
+        "scale_model_score" : 1.0, 
+} 
