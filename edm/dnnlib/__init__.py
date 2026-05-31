7ce297ed1ac7131241d36c9abfcec1e06e56b00a
Brandon Chin-A-Lien <bchinalien@outlook.com>
2026-05-31 15:13:19 +0200

    Added __init__ file for simplifying imports

---
+++ edm/dnnlib/__init__.py
@@ -0,0 +1,12 @@
+from .util import (
+    EasyDict,
+    Logger,
+    set_cache_dir,
+    make_cache_dir_path,
+    open_url,
+    import_net_from_url,
+    is_url,
+    get_obj_by_name,
+    call_func_by_name,
+    construct_class_by_name,
+)
